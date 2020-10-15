2ce426833714504e782a2e23f2492d1f6fa41086
SquirrelPaul <39982829+SquirrelPaul@users.noreply.github.com>
2020-10-15 13:48:17 +0800

    Update [Gameres]Gameres新游榜单按日期爬取.py

--- [Gameres]Gameres新游榜单按日期爬取.py
+++ [Gameres]Gameres新游榜单按日期爬取.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 13 21:46:13 2020
-
-@author: Administrator
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 21 16:00:44 2020
+Created on Sun Sep 27 10:10:45 2020
 
 @author: liuwp
-
-MyanimeList scramper
 """
-
-# -*- coding: utf-8 -*-
-
 
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import time
+import datetime
 import json
-###-----------------------------------
+###修改日期和数据时长-----------------------------------
+t_str ='2020-06-01' #起始日期
+l = 50 #数据时长，覆盖3k天 
+###----------------------------------------------------
 
-###------------------------------------
+#日期
+
+datelist=[]
+d = datetime.datetime.strptime(t_str,'%Y-%m-%d')
+delta = datetime.timedelta(days=3)
+datelist.append(d)
+for i in range(0,l):   
+    d=d+delta
+    datelist.append(d)
+
 
 #定义函数-获取网页
 def getHtmlText(url):
@@ -61,16 +61,13 @@ def parText(result,text):
     
     result.append([Time, Title, Type, State, Producer])
 
-
-#date =['2019-09-01','2019-09-04','2019-09-07','2019-09-10','2019-09-13','2019-09-16','2019-09-19','2019-09-22','2019-09-25','2019-09-28','2019-10-01','2019-10-04','2019-10-07','2019-10-10','2019-10-13','2019-10-16','2019-10-19','2019-10-22','2019-10-25','2019-10-28','2019-11-01','2019-11-04','2019-11-07','2019-11-10','2019-11-13','2019-11-16','2019-11-19','2019-11-22','2019-11-25','2019-11-28','2019-12-01','2019-12-04','2019-12-07','2019-12-10','2019-12-13','2019-12-16','2019-12-19','2019-12-22','2019-12-25','2019-12-28','2020-01-01','2020-01-04','2020-01-07','2020-01-10','2020-01-13','2020-01-16','2020-01-19','2020-01-22','2020-01-25','2020-01-28','2020-02-01','2020-02-04','2020-02-07','2020-02-10','2020-02-13','2020-02-16','2020-02-19','2020-02-22','2020-02-25','2020-02-28','2020-03-02','2020-03-05','2020-03-08','2020-03-11','2020-03-14','2020-03-17','2020-03-25','2020-03-30','2020-04-15','2020-05-15']
-date =['2019-10-05'] #需要爬哪些日期？
 result = []
 url = "https://www.gameres.com/newgame"
 
 #主程序，循环
 print(url)
-for k in range(0,len(date)):
-    data = {'tdate':date[k],'dataType':'fragment'}  #发送参数请求，读取制定日期的数据（showm)
+for k in range(0,len(datelist)):
+    data = {'tdate':datelist[k],'dataType':'fragment'}  #发送参数请求，读取制定日期的数据（showm)
 #    html2 = json.loads(getHtmlText(url))
     html = json.loads(getHtmlText(url))['html']    #参数请求返回的json格式，编为字典并提取其中的html字段即可解析
     soup = BeautifulSoup(html,'html.parser')
@@ -85,4 +82,4 @@ for k in range(0,len(date)):
                 continue
     print(k)
 
-pd.DataFrame(result, columns=['所属时间', '游戏名称', '游戏类型', '状态', '制作组']).to_excel('E:\Gameres_Newgame669.xlsx', index=None, encoding='utf-8')  # 【指定字段写入的规则】以sep的属性作为分隔符，将文件写入excel文件中
+pd.DataFrame(result, columns=['所属时间', '游戏名称', '游戏类型', '状态', '制作组']).to_excel('E:\Gameres_Newgame927.xlsx', index=None, encoding='utf-8')  # 【指定字段写入的规则】以sep的属性作为分隔符，将文件写入excel文件中
